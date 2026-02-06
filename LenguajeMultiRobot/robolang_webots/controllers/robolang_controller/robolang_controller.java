bb6dc5be04d8bc365b99fbc6d706a3522fe8e34c
shiguera <shiguera@telefonica.net>
2026-02-06 12:38:27 +0100

    Intentando habilitar controlador externo e interno para webots, pero el interno no funciona. Webots no admite que el controlador tenga definido un paquete y eso no me deja desarrollarlo como proyecto java con todo lo demás, así que hay que ejecutar el controlador como externo. Ahora hay dos controladores Webots:  - robolang_controller: arranca directamente el fichero instrucciones.txt  - robolang_controler_externo: ofrece un menú para elegir el fichero del    código fuente y el controlador. Esta disposición no es correcta y habrá que cambiarla,    sacándola del directorio del controlador webots y dejándola fuera, para    que tenga sentido elegir el controlador de Webots u otro.

--- LenguajeMultiRobot/robolang_webots/controllers/robolang_controller/robolang_controller.java
+++ LenguajeMultiRobot/robolang_webots/controllers/robolang_controller/robolang_controller.java
@@ -1,57 +1,23 @@
 package robolang_webots.controllers.robolang_controller;
 
 import java.nio.file.Path;
-import java.util.Scanner;
-
 import lenguaje.Interprete;
 import lenguaje.RoboLangBaseListener;
 
 
 public class robolang_controller {
    
-   private static Scanner scanner;
    public static void main(String[] args) throws Exception {
 
       Path workingDirectory = Path.of(System.getProperty("user.dir"));
       System.out.println("Working Directory = " + workingDirectory.toAbsolutePath());
       
-      scanner = new Scanner(System.in);
-
-      String fileName = readNombreFichero();
-      RoboLangBaseListener listener = readRobot();      
+      String fileName = "instrucciones.txt";
+      RoboLangBaseListener listener = new AdaptadorWebots();      
 
       Interprete interprete = new Interprete(listener);
-      interprete.ejecutaPrograma(fileName);
-      
-      scanner.close();
+      interprete.ejecutaPrograma(fileName);     
    }
 
-   private static RoboLangBaseListener readRobot() {
-      RoboLangBaseListener listener = null;
-      
-      System.out.println("\nOpciones de Robot");
-      System.out.println("   1.- Robot Webots");
-      System.out.println("   2.- Robot de consola");
-      System.out.print("Su opción: ");
-      int opcion = scanner.nextInt();
-      switch (opcion) {
-         case 1:
-            listener = new AdaptadorWebots();
-            break;
-         case 2:
-            //listener = new RobotConsolaListener();
-            break;
-         default:
-            //listener = new RobotConsolaListener();
-            break;
-      }
-      return listener;
-   }   
-
-   private static String readNombreFichero() {
-      System.out.print("\nNombre del fichero con el código fuente: ");
-      String fileName = scanner.nextLine();
-      return fileName;
-   }
 
 }
---
+++ LenguajeMultiRobot/robolang_webots/controllers/robolang_controller/robolang_controller_externo.java
@@ -0,0 +1,57 @@
+package robolang_webots.controllers.robolang_controller;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+
+import lenguaje.Interprete;
+import lenguaje.RoboLangBaseListener;
+
+
+public class robolang_controller_externo {
+   
+   private static Scanner scanner;
+   public static void main(String[] args) throws Exception {
+
+      Path workingDirectory = Path.of(System.getProperty("user.dir"));
+      System.out.println("Working Directory = " + workingDirectory.toAbsolutePath());
+      
+      scanner = new Scanner(System.in);
+
+      String fileName = readNombreFichero();
+      RoboLangBaseListener listener = readRobot();      
+
+      Interprete interprete = new Interprete(listener);
+      interprete.ejecutaPrograma(fileName);
+      
+      scanner.close();
+   }
+
+   private static RoboLangBaseListener readRobot() {
+      RoboLangBaseListener listener = null;
+      
+      System.out.println("\nOpciones de Robot");
+      System.out.println("   1.- Robot Webots");
+      System.out.println("   2.- Robot de consola");
+      System.out.print("Su opción: ");
+      int opcion = scanner.nextInt();
+      switch (opcion) {
+         case 1:
+            listener = new AdaptadorWebots();
+            break;
+         case 2:
+            //listener = new RobotConsolaListener();
+            break;
+         default:
+            //listener = new RobotConsolaListener();
+            break;
+      }
+      return listener;
+   }   
+
+   private static String readNombreFichero() {
+      System.out.print("\nNombre del fichero con el código fuente: ");
+      String fileName = scanner.nextLine();
+      return fileName;
+   }
+
+}
