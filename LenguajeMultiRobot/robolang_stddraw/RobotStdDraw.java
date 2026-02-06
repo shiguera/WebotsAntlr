1bcaef85d9d95402b6554c768f1fe40c59d86cf3
shiguera <shiguera@mercatorlab.com>
2026-02-06 23:24:36 +0100

    Corrijo bug en el cálculo de razones trigonométricas de RobotStdDraw.java
    Estaba calculando los Math.sin() y Math.cos() de ángulos medidos
    en grados. He añadido una función deg2rad() y he corregido.

--- LenguajeMultiRobot/robolang_stddraw/RobotStdDraw.java
+++ LenguajeMultiRobot/robolang_stddraw/RobotStdDraw.java
@@ -3,7 +3,7 @@ package robolang_stddraw;
 public class RobotStdDraw {
 
 	private Posicion posicion;
-	private double velocidad = 0.0;
+	private double velocidad;
 	private double incremento_tiempo = 0.25;
 
 	public RobotStdDraw () {
@@ -11,28 +11,32 @@ public class RobotStdDraw {
 
 	}
 	private void initMundo() {
+		posicion = new Posicion();
+		velocidad = 0.0;
+
 		double Tam = 5;
 		StdDraw.setXscale (0, Tam);
 		StdDraw.setYscale (0, Tam);
 		StdDraw.setPenRadius (0.005);
 		StdDraw.setPenColor (StdDraw.BLUE);
-		posicion = new Posicion();
 		draw(null, posicion);
 		StdDraw.show();
 	}
 
 	public void girar(double angulo) {
+		System.out.println("Rumbo: " + posicion.rumbo + " angulo: " + angulo);
 		posicion.rumbo += angulo;
 		posicion.rumbo = normalize(posicion.rumbo);
+		System.out.println("Rumbo: " + posicion.rumbo + " angulo: " + angulo);
 	}
 
 	public void avanzar(double velocidad, double dist) {
 		this.velocidad = velocidad;
 		double dist_recorrida = 0.0;
 		while(dist_recorrida < dist) {
-			double d = velocidad * incremento_tiempo; 
-			double incx = d * Math.cos(posicion.rumbo);
-			double incy = d * Math.sin(posicion.rumbo);
+			double d = this.velocidad * incremento_tiempo; 
+			double incx = d * Math.cos(deg2rad(posicion.rumbo));
+			double incy = d * Math.sin(deg2rad(posicion.rumbo));
 			dist_recorrida += Math.sqrt(incx*incx + incy*incy);
 			Posicion anterior = posicion;
 			posicion.x += incx;
@@ -64,5 +68,8 @@ public class RobotStdDraw {
 	        angulo += dosPi;
 	    }
 	    return angulo;
+	}
+	public double deg2rad(double angulo) {
+		return angulo*Math.PI / 180.0;
 	}
 }
