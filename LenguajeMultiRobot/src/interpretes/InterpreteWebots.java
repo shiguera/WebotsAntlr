f97de681f1ec6074e332aad7588fc263ad6f155a
shiguera <shiguera@telefonica.net>
2026-02-08 12:39:03 +0100

    Mejoro el script de arranque de Webots
    Ahora, detecta si Webots está ya arrancado y pemite un parámetro
    con el nombre del fichero fuente, que se lo pasa a InterpreteWebots.java.
    
    InterpreteWebots.java permite recibir un parámetro con el nombre
    del fichero fuente a ejecutar. Si no recibe el parámetro, ejecuta
    el fichero programa_1.rbl.
    
    He cambiado la extensión de los ficheros fuente a .rbl

--- LenguajeMultiRobot/src/interpretes/InterpreteWebots.java
+++ LenguajeMultiRobot/src/interpretes/InterpreteWebots.java
@@ -1,24 +1,22 @@
 package interpretes;
 
-import java.nio.file.Path;
-
 import lenguaje.RoboLangBaseListener;
 import robolang_webots.WebotsListener;
 
-
 public class InterpreteWebots {
-   
+
    public static void main(String[] args) throws Exception {
 
-      Path workingDirectory = Path.of(System.getProperty("user.dir"));
-      System.out.println("Working Directory = " + workingDirectory.toAbsolutePath());
-      
-      String fileName = "instrucciones.txt";
-      RoboLangBaseListener listener = new WebotsListener();      
+      String programaFuente = "programa_1.rbl";
+      if (args.length == 1) {
+         programaFuente = new String(args[0]);
+      }
 
+      RoboLangBaseListener listener = new WebotsListener();
       Interprete interprete = new Interprete(listener);
-      interprete.ejecutaPrograma(fileName);     
-   }
 
+      System.out.println("🚀 InterpreteWebots ejecutando: " + programaFuente);
+      interprete.ejecutaPrograma(programaFuente);
+   }
 
 }
